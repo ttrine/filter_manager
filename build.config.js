225df59ff7fd94ccef3c800cf53b2dbf57f386d0
Crystal Huang <crystal.huang@chuang-mbp17.corp.1010data.com>
2017-06-27 11:00:03 -0400

    create unique leaves constraint and columns to show access to children

--- build.config.js
+++ build.config.js
@@ -276,7 +276,7 @@ module.exports = {
 			dest: '<%= build_dir %>/app/view.xml',  // tokenize src to dest using 'template' task
 			title: 'Filter Manager',
 			name: '<%= root_path %>.view',
-			url: 'https://www2.1010data.com/cgi-bin/beta-latest/quickapp?path=<%= quick_queries.view.name %>',
+			url: 'https://www2.1010data.com/cgi-bin/prime-latest/quickapp?path=<%= quick_queries.view.name %>',
 			table: '<%= quick_queries.view.name %>\(<%= quick_queries.view.title %>\;\;\)=<%= basetable %>',
 			ordinal: 99 // deploy last because there are dependencies which must be deployed beforehand
 		},
@@ -286,7 +286,7 @@ module.exports = {
 			dest: '<%= build_dir %>/app/model.xml',  // tokenize src to dest using 'template' task
 			title: 'Model',
 			name: '<%= root_path %>.model',
-			url: 'https://www2.1010data.com/cgi-bin/beta-latest/quickapp?path=<%= quick_queries.model.name %>',
+			url: 'https://www2.1010data.com/cgi-bin/prime-latest/quickapp?path=<%= quick_queries.model.name %>',
 			table: '<%= quick_queries.model.name %>\(<%= quick_queries.model.title %>\;\;\)=<%= basetable %>'
 		},
 
@@ -295,7 +295,7 @@ module.exports = {
 			dest: '<%= build_dir %>/app/controller.xml',  // tokenize src to dest using 'template' task
 			title: 'Controller',
 			name: '<%= root_path %>.controller',
-			url: 'https://www2.1010data.com/cgi-bin/beta-latest/quickapp?path=<%= quick_queries.controller.name %>',
+			url: 'https://www2.1010data.com/cgi-bin/prime-latest/quickapp?path=<%= quick_queries.controller.name %>',
 			table: '<%= quick_queries.controller.name %>\(<%= quick_queries.controller.title %>\;\;\)=<%= basetable %>'
 		}
 	},
@@ -304,7 +304,7 @@ module.exports = {
 		developer: {
             root_path: 'x1010data.huangc.filter_manager_dev',
             login: {
-                gateway: 'https://www2.1010data.com/beta-latest/gw',
+                gateway: 'https://www2.1010data.com/prime-latest/gw',
                 id: '<%= login_id %>',
                 password: '<%= login_password %>'
             }
@@ -313,7 +313,7 @@ module.exports = {
 		test: {
 			root_path: 'product_filter_manager',
 			login: {
-				gateway: 'http://njtest.corp.1010data.com/beta-latest/gw',
+				gateway: 'http://njtest.corp.1010data.com/prime-latest/gw',
 				id: '<%= login_id %>',
 				password: '<%= login_password %>'
 			}
@@ -322,7 +322,7 @@ module.exports = {
 		prod: {
 			root_path: 'product_filter_manager',
 			login: {
-				gateway: 'https://www2.1010data.com/beta-latest/gw',
+				gateway: 'https://www2.1010data.com/prime-latest/gw',
 				id: '<%= login_id %>',
 				password: '<%= login_password %>'
 			}
